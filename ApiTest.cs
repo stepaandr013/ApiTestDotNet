5bf57e9ceabe9d83bd1ba773db63525f0f8ffa94
stepaandr013 <stepa.andr@gmail.com>
2022-09-02 08:58:18 +0300

    Initial commit

---
+++ ApiTest.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTestsDotNet
+{
+    public class ApiTest
+    {
+        [Test]
+        public async Task GetUsers()
+        {
+            
+            HttpClient httpClient = new HttpClient();
+            string url = "https://reqres.in/api/users?page=2";
+
+            HttpResponseMessage responseMessage = (await httpClient.GetAsync(url))
+                .EnsureSuccessStatusCode();
+
+            string responseBody = await responseMessage.Content.ReadAsStringAsync();
+            JObject keyValuePairs = JObject.Parse(responseBody);
+
+            JToken jToken = keyValuePairs["data"];
+
+            List<UserPojo> users = jToken.ToObject<List<UserPojo>>();
+
+            Assert.IsTrue(responseMessage.IsSuccessStatusCode);
+
+            for(int i = 0; i < users.Count; i++)
+            {
+                Assert.IsTrue(users[i].email.ToLower().Contains(users[i].first_name.ToLower()));
+                Assert.IsTrue(users[i].email.ToLower().Contains(users[i].last_name.ToLower()));
+                Assert.IsTrue(users[i].avatar.Contains(users[i].id.ToString()));
+            }
+
+
+        }
+    }
+}
